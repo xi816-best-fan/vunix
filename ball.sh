95d24d27355ab4d2bfde4088805bea739971f389
sigxc <sigxc@FourthTemple>
2025-04-09 23:53:48 +0300

    improved

--- ball.sh
+++ ball.sh
@@ -1,2 +1,4 @@
-gcc -o tinyboot tinyboot.c
-gcc -o kernel.elf kernel.c
+#!/bin/bash
+
+clang -O2 -march=native -mtune=native -o tinyboot tinyboot.c
+clang -O2 -march=native -mtune=native -o kernel.elf kernel.c
